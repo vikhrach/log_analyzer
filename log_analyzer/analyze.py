fd85b482de6cd1f58e65e889e6737c434d23b132
Vadim Vikhrachev <vikhrachev@gmail.com>
2024-12-09 00:35:15 +0300

    add types

--- log_analyzer/analyze.py
+++ log_analyzer/analyze.py
@@ -1,12 +1,12 @@
-import gzip
 import datetime
-import re
+import gzip
 import os
+import re
 
 config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
-def extract_date_from_filename(filename):
+def extract_date_from_filename(filename: str) -> datetime.datetime | None:
     pattern = r"nginx-access-ui\.log-(\d{8})\.*"
     match = re.search(pattern, filename)
     if match:
@@ -15,19 +15,20 @@ def extract_date_from_filename(filename):
         return None  # If no match is found
 
 
-def search_latest_logfile(log_dir):
+def search_latest_logfile(log_dir: str) -> str:
     maxdate = datetime.datetime.fromtimestamp(0)
-    maxpath = ""
     for i in os.listdir(log_dir):
         curpath = os.path.join(log_dir, i)
         curdate = extract_date_from_filename(curpath)
+        if not curdate:
+            break
         if curdate > maxdate:
             maxdate = curdate
             maxpath = curpath
-    return curpath
+    return maxpath
 
 
-def get_request_time_generator(file_path):
+def get_request_time_generator(file_path: str):
     # Check if the file is a GZIP file
     with (
         gzip.open(file_path, "rt")
@@ -42,8 +43,8 @@ def get_request_time_generator(file_path):
             yield (match.group("url"), match.group("request_time"))
 
 
-def collect_data(generator):
-    raw_data = {}
+def collect_data(generator) -> tuple[dict[str,list[float]], int, float]:
+    raw_data:dict[str,list[float]] = {}
     general_count = 0
     general_time = 0.0
     for url, request_time in generator:
@@ -54,28 +55,31 @@ def collect_data(generator):
     return (raw_data, general_count, general_time)
 
 
-def get_statistics(data):
+def get_statistics(data, all_count, all_time):
 
     statistics = {}
-    for k, v in data[0].items():
+    for k, v in data.items():
         count = len(v)
-        time_sum = sum(v)
+        time_sum_per_url = sum(v)
         statistics[k] = {
             "count": count,
-            "count_perc": count / data[1],
-            "time_sum": time_sum,
-            "time_perc": time_sum / data[2],
-            "time_avg": time_sum / count,
+            "count_perc": count / all_count,
+            "time_sum": time_sum_per_url,
+            "time_perc": time_sum_per_url / all_time,
+            "time_avg": time_sum_per_url / count,
             "time_max": max(v),
             "time_min": min(v),
         }
     return statistics
 
 
-log_file = search_latest_logfile(config["LOG_DIR"])
-request_time_generator = get_request_time_generator(log_file)
-raw_data = collect_data(request_time_generator)
-analyzed_data = get_statistics(raw_data)
+try:
+    log_file = search_latest_logfile(str(config["LOG_DIR"]))
+    request_time_generator = get_request_time_generator(log_file)
+    raw_data = collect_data(request_time_generator)
+    analyzed_data = get_statistics(*raw_data)
+except FileExistsError as err:
+    structlog
 print(log_file)
-print(extract_date_from_filename(log_file))
+# print(extract_date_from_filename(log_file))
 print(analyzed_data)
